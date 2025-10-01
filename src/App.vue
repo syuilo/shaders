08cf5f0d988a27a7dfa2f93fccd4ff79521a4c22
syuilo <4439005+syuilo@users.noreply.github.com>
2025-10-01 14:36:33 +0900

    ✌️

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+<ShadersCyber />
+</template>
+
+<script lang="ts" setup>
+import ShadersCyber from './components/ShadersCyber.vue';
+</script>
+
+<style scoped>
+
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import * as Path from 'node:path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	base: '/shaders/',
+	plugins: [vue()],
+	resolve: {
+		alias: {
+			'@': Path.resolve('src'),
+		},
+	},
+})
