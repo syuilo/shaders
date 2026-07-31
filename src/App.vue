b48267fc58d9423fba76696219bb78c62ab40185
syuilo <4439005+syuilo@users.noreply.github.com>
2026-07-31 22:01:06 +0900

    feat: render generated shader list

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,21 @@
 <XPlayground v-if="name != null" :name="name"/>
 <div v-else>
 	<h1>syuilo's Shader Playground</h1>
-	<!-- TODO: render list of shaders -->
+	<ul>
+		<li v-for="shader in shaders" :key="shader">
+			<a :href="`?${shader}`">{{ shader }}</a>
+		</li>
+	</ul>
 </div>
 </template>
 
 <script lang="ts" setup>
-import { defineAsyncComponent, ref } from 'vue';
+import { ref } from 'vue';
 import XPlayground from './playground.vue';
+
+const shaders = Object.keys(import.meta.glob('./shaders/*/main.ts'))
+	.map(path => path.split('/')[2])
+	.sort();
 
 const name = ref(window.location.search.match(/\?([^&]+)/)?.[1] ?? null);
 </script>
