a7c7fd7090a94ed7167373baed81b87f4814f9ef
syuilo <4439005+syuilo@users.noreply.github.com>
2026-08-10 07:26:31 +0900

    Update main.ts

--- src/shaders/cyber/main.ts
+++ src/shaders/cyber/main.ts
@@ -230,7 +230,7 @@ export const playground = definePlayground({
 		] : [];
 
 		const symbolTextures = await createTextureFromImages(wgpu.device, symbolTextureUrls, {
-			mips: false, // 有効にすると特定条件下(marginが0)で画像の端に線が入ってしまう あとにじむ
+			mips: true,
 		});
 
 		const sourceTexture = createTextureFromSource(wgpu.device, params.source?.element ?? [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], {
